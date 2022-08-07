2fce8878729abe567cdd374417c87c54fd80473b
dqoborsi <dqoborsi@gmail.com>
2022-08-06 17:33:53 -0700

    assignment complete

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
 from stories import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def index():
-  return render_template('madlibfe.html')
+  return render_template('madlibfe.html', prompts=story.prompts)
+
+@app.route('/story')
+def display_story():
+
+  text = story.generate(request.args)
+
+  return render_template("story.html", text = text)
